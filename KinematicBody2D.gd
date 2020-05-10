f315a2dc2509921451ca2346cbd85be1cf9cb5ff
Caio H. Reblin <caioreblin07@gmail.com>
2020-05-10 17:54:04 -0300

    Desenvolvido comportamentos de movimentação.

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,22 +1,81 @@
 extends KinematicBody2D
 
-var gas = 600
+var gas = 500
 var gasCost = 1
 var motion = Vector2()
-var speed = 500
-var speedCostAfterEmptyGas = 1
+var jump = 10
+var speed = 0
+var speedCostAfterEmptyGas = 5
+var stop
 
-func _process(delta):
-	motion.y += 10
+func _physics_process(delta):
 	
-	if Input.is_action_pressed("ui_right") && speed > 0:
-		motion.x = speed
-		if gas > 0:
-			gas -= gasCost	
-		else:
-			speed -= speedCostAfterEmptyGas
-	else:
-		motion.x = 0
+	#seta a altura
+	motion.y = jump
 	
+	#lógica de pulo			
+	if Input.is_action_pressed("ui_up"):			
+		jump = 100
+	
+	#lógica para movimentação
+	move(delta);
+	
+	#lógica para parar movimentação
+	stop(delta);
+				
+	#lógica para manter o personagem parado caso nenhuma tecla esteja pressionada			
+	no_action(delta)
+				
+	#mover camera de acordo com o player
 	move_and_slide(motion)
+	pass
+	
+func move(delta):
+	#verifica se o jogador esta pressionando pra andar
+	if Input.is_action_pressed("ui_right"):
+		
+		#seta a velocidade
+		motion.x = speed
+		
+		#aumenta a velocidade no inicio até 500
+		if speed < 500 and gas > 0 :
+			speed += 5;
+		
+		#lógica de diminuição de gas	
+		#lógica de inércia de velocidade quando o gás acabar		
+		if speed > 0:
+			if gas > 0:
+				gas -= gasCost	
+			else:
+				speed -= speedCostAfterEmptyGas	
+	pass
+				
+func stop(delta):
+	#verifica se o jogador esta pressionando pra andar
+	if Input.is_action_pressed("ui_left"):
+		
+		#seta a velocidade
+		motion.x = speed
+		
+		#diminui velocidade de movimento até chegar a zero
+		if speed > 0 :
+			speed -= 20;
+		else:
+			speed = 0	
+			
+	pass
+				
+func no_action(delta) :
+	if !Input.is_action_pressed("ui_right") and !Input.is_action_pressed("ui_left") :				
+		motion.x = speed
+		
+		if speed > 0 :
+			speed -= 10
+		else:
+			speed = 0
+		
+	pass
+				
+func jump(delta):
+	
 	pass
